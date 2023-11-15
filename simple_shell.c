bff31822a99477cf1cb6f49c198c2c6c5441b2eb
ItranscendD <danielcasey988@gmail.com>
2023-11-15 04:23:41 -0800

    functioning shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,71 +1,79 @@
+#include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-
-#define MAX_INPUT_SIZE 100
-
-/**
- * display_prompt - Displays the shell prompt.
- */
-void display_prompt(void)
-{
-	printf("simple_shell> ");
-}
+#include <signal.h>
 
 /**
- * main - Entry point for the simple shell program.
+ * main - Simple UNIX command line interpreter
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char input[MAX_INPUT_SIZE];
+	char *command;
+	size_t len = 0;
+	ssize_t read;
+
+	signal(SIGINT, SIG_IGN);
 
 	while (1)
 	{
-		display_prompt();
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
+		read = getline(&command, &len, stdin);
 
-		/* Read user input */
-		if (fgets(input, sizeof(input), stdin) == NULL)
+		if (read == -1)
 		{
-			/* Handle end of file (Ctrl+D) */
-			printf("\n");
-			break;
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			free(command);
+			exit(EXIT_SUCCESS);
 		}
 
-		/* Remove the newline character at the end */
-		input[strcspn(input, "\n")] = 0;
+		if (_strcmp(command, "exit\n") == 0)
+		{
+			free(command);
+			exit(EXIT_SUCCESS);
+		}
 
-		/* Fork a child process */
-		pid_t pid = fork();
-
-		if (pid == -1)
-		{
-			/* Forking error */
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			/* Child process */
-			/* Execute the command using execve */
-			if (execlp(input, input, (char *)NULL) == -1)
-			{
-				/* Print an error message if the command is not found */
-				perror("execve");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			/* Parent process */
-			/* Wait for the child process to finish */
-			int status;
-			waitpid(pid, &status, 0);
-		}
+		execute_command(command);
 	}
 
 	return (0);
+}
+
+/**
+ * execute_command - Execute a command
+ * @command: The command to execute
+ */
+void execute_command(char *command)
+{
+	pid_t child_pid;
+	int status;
+
+	command[_strlen(command) - 1] = '\0'; /* Remove trailing newline */
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+
+	if (child_pid == 0)
+	{
+		/* Child process */
+		if (execve(command, NULL, NULL) == -1)
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/* Parent process */
+		wait(&status);
+	}
 }
